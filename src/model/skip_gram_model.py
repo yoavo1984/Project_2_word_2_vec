40f946dec5555f3e0d0afb4ba6051f360d52c77b
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-05-17 16:14:22 +0300

    SkipGramModel : class init is implemented. Unigram class now accept an alpha parameter for the noise distribution.

--- src/model/skip_gram_model.py
+++ src/model/skip_gram_model.py
@@ -1,13 +1,36 @@
 import numpy as np
 
 class SkipGramModel(object):
-    def __init__(self, hyperparameters, training_data):
-        pass
-        # Create a vector for each word in the training data (u_i,, v_i)
+    def __init__(self, hyperparameters, training_corpus):
+        self.corpus = training_corpus
 
-        # Sample each vector from a multivariative Gaussian
+        # Building the noise distribution.
+        self.unigram = UniGram(training_corpus)
+        self.unigram.set_alpha(hyperparameters.noise_d)
+
+        self.k = hyperparameters.k
+
+        # Create a vector for each word in the training data (u_i, v_i), and Sample each vector from a
+        # multivariate Gaussian
+        number_of_words = training_corpus.get_words_count()
+        self.target_vectors = np.random.normal(0, 0.01, size=(number_of_words, hyperparameters.d))
+        self.context_vectors = np.random.normal(0, 0.01, size=(number_of_words, hyperparameters.d))
 
         # Normalize each vector (L2 Norm).
+        self.normalize_vectors()
+
+    def normalize_vectors(self):
+        """
+        Normalize all the model vectors(target and context) to have a norm of 1.
+        Returns:
+
+        """
+        target_norms = np.linalg.norm(self.target_vectors, axis=1, keepdims=True)
+        self.target_vectors = self.target_vectors / target_norms
+
+        context_norms = np.linalg.norm(self.context_vectors, axis=1, keepdims=True)
+        self.context_vectors = self.context_vectors / context_norms
+
 
     def sample_word(self, alpha=1):
         """
@@ -18,7 +41,7 @@ class SkipGramModel(object):
         Returns:
             A word sampled using the unigram to power alpha distribution
         """
-        pass
+        return self.unigram.sample_word()
 
     def sample_k_words(self):
         """
@@ -27,22 +50,26 @@ class SkipGramModel(object):
         Returns:
             (list) k words sampled from the hyperparameters noise distribution
         """
-        pass
+        samples = []
+        for i in range (self.k):
+            samples.append(self.sample_word())
 
-        def compute_log_probability(word_i, word_j, words_k):
-            """
-            Computes the log probability of word_i given word_j and K negative sampled
-            words.
-            Args:
-                word_i: Target word. 
-                word_j: Given context word
-                words_k: K sampled negative words
+        return samples
 
-            Returns:
-                The log probablity of the given Target word given the context word and K
-                negative sampled words
-            """
-            return 0.0
+    def compute_log_probability(word_i, word_j, words_k):
+        """
+        Computes the log probability of word_i given word_j and K negative sampled
+        words.
+        Args:
+            word_i: Target word. 
+            word_j: Given context word
+            words_k: K sampled negative words
+
+        Returns:
+            The log probablity of the given Target word given the context word and K
+            negative sampled words
+        """
+        return 0.0
 
 class UniGram(object):
     def __init__(self, corpus):
@@ -71,7 +98,10 @@ class UniGram(object):
         for key, val in self.distribution.items():
             self.distribution[key] = val / num_of_words
 
-    def ger_random_word(self):
+    def reset_probabilites(self):
+        self.generate_unigram_distribution()
+
+    def sample_word(self):
         """
         Returns a random word from the corpus using a unigram distribution.
         Returns:
@@ -82,10 +112,23 @@ class UniGram(object):
 
         return np.random.choice(words, p=p)
 
+    def set_alpha(self, alpha):
+        self.reset_probabilites()
+        probabilites_sum = 0
+
+        for key,val in self.distribution.items():
+            new_value = pow(val, alpha)
+            self.distribution[key] = new_value
+            probabilites_sum += new_value
+
+        for key,val in self.distribution.items():
+            self.distribution[key] = val / probabilites_sum
+
+
 if __name__ == "__main__":
     class mock_corpus():
         def __init__(self):
-            self.str = "This is a long list of words you can use is is is is is is is is is"
+            self.str = "is is is is is is is is is is is is one"
 
         def get_number_of_words(self):
             return len(self.str.split())
@@ -97,5 +140,18 @@ if __name__ == "__main__":
 
     corpus = mock_corpus()
     uni = UniGram(corpus)
+
+    print(" -- Unigram Sampling --")
     for i in range(10):
-        print(uni.ger_random_word())
+        print(uni.sample_word())
+
+    uni.set_alpha(0)
+
+    print("\n -- Uniform Sampling --")
+    for i in range(10):
+        print(uni.sample_word())
+
+    uni.set_alpha(0.5)
+    print("\n -- alpha=0.5 Sampling --")
+    for i in range(10):
+        print(uni.sample_word())
