90868d0467969f7339405221be9cc5f9f47c68fe
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-05-24 16:41:33 +0300

    Created the learning module.

---
+++ src/learning/gradient_descent.py
@@ -0,0 +1,9 @@
+NUMITER = 20000
+
+class GradientDescent():
+    def __init__(self, hyperparameters):
+        pass
+
+    def learnParamsUsingSGD(self, model, training_corpus):
+        for i in range(0, 20000):
+            pass
---
+++ src/learning/learning_hyperparameters.py
@@ -0,0 +1,6 @@
+class LearningHyperparameters():
+    def __init__(self, learning_rate, batch_size, decay_rate):
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.decay_rate = decay_rate
+        
