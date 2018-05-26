a7fe625e643dda1edee21dcbf97b2d4913dda7d9
YoavOrlev <yoav.orlev@milab.idc.ac.il>
2018-05-26 09:29:57 +0300

    Learning hyperparameters : Added the print iterations variable.

--- src/learning/learning_hyperparameters.py
+++ src/learning/learning_hyperparameters.py
@@ -1,6 +1,6 @@
 class LearningHyperparameters():
-    def __init__(self, learning_rate, batch_size, decay_iteration_num):
+    def __init__(self, learning_rate, batch_size, decay_iteration_num, print_likelihood_iterations=0):
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.decay_iteration_num = decay_iteration_num
-        
+        self.print_likelihood_iterations = print_likelihood_iterations
